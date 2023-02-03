e4354e8c34b29f2b6b9833b50db7f28a4b0d21da
Mikhail <121482778+chikhanov@users.noreply.github.com>
2023-02-03 11:59:00 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '6056449564:AAFogIp2LOzUsDupkqjqAcOu3ySFZjaaSfw'
+
+keys = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        'атом': 'ATOM',
+        'евро': 'EUR',
+        }
